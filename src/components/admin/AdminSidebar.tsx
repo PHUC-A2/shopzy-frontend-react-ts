ea5220c858b0255ab556580b520ddf9fd99e42ec
PHUC-A2 <phucban297@gmail.com>
2025-08-13 02:30:11 +0700

    #7 update overflow-y: auto; cho Sidebar

--- src/components/admin/AdminSidebar.tsx
+++ src/components/admin/AdminSidebar.tsx
@@ -42,6 +42,193 @@ const items: MenuItem[] = [
             },
         ],
     },
+    {
+        key: 'sub2',
+        label: 'Navigation Two',
+        icon: <AppstoreOutlined />,
+        children: [
+            { key: '9', label: 'Option 9' },
+            { key: '10', label: 'Option 10' },
+            {
+                key: 'sub3',
+                label: 'Submenu',
+                children: [
+                    { key: '11', label: 'Option 11' },
+                    { key: '12', label: 'Option 12' },
+                ],
+            },
+        ],
+    },
+    {
+        key: 'sub2',
+        label: 'Navigation Two',
+        icon: <AppstoreOutlined />,
+        children: [
+            { key: '9', label: 'Option 9' },
+            { key: '10', label: 'Option 10' },
+            {
+                key: 'sub3',
+                label: 'Submenu',
+                children: [
+                    { key: '11', label: 'Option 11' },
+                    { key: '12', label: 'Option 12' },
+                ],
+            },
+        ],
+    },
+    {
+        key: 'sub2',
+        label: 'Navigation Two',
+        icon: <AppstoreOutlined />,
+        children: [
+            { key: '9', label: 'Option 9' },
+            { key: '10', label: 'Option 10' },
+            {
+                key: 'sub3',
+                label: 'Submenu',
+                children: [
+                    { key: '11', label: 'Option 11' },
+                    { key: '12', label: 'Option 12' },
+                ],
+            },
+        ],
+    },
+    {
+        key: 'sub2',
+        label: 'Navigation Two',
+        icon: <AppstoreOutlined />,
+        children: [
+            { key: '9', label: 'Option 9' },
+            { key: '10', label: 'Option 10' },
+            {
+                key: 'sub3',
+                label: 'Submenu',
+                children: [
+                    { key: '11', label: 'Option 11' },
+                    { key: '12', label: 'Option 12' },
+                ],
+            },
+        ],
+    },
+    {
+        key: 'sub2',
+        label: 'Navigation Two',
+        icon: <AppstoreOutlined />,
+        children: [
+            { key: '9', label: 'Option 9' },
+            { key: '10', label: 'Option 10' },
+            {
+                key: 'sub3',
+                label: 'Submenu',
+                children: [
+                    { key: '11', label: 'Option 11' },
+                    { key: '12', label: 'Option 12' },
+                ],
+            },
+        ],
+    },
+    {
+        key: 'sub2',
+        label: 'Navigation Two',
+        icon: <AppstoreOutlined />,
+        children: [
+            { key: '9', label: 'Option 9' },
+            { key: '10', label: 'Option 10' },
+            {
+                key: 'sub3',
+                label: 'Submenu',
+                children: [
+                    { key: '11', label: 'Option 11' },
+                    { key: '12', label: 'Option 12' },
+                ],
+            },
+        ],
+    },
+    {
+        key: 'sub2',
+        label: 'Navigation Two',
+        icon: <AppstoreOutlined />,
+        children: [
+            { key: '9', label: 'Option 9' },
+            { key: '10', label: 'Option 10' },
+            {
+                key: 'sub3',
+                label: 'Submenu',
+                children: [
+                    { key: '11', label: 'Option 11' },
+                    { key: '12', label: 'Option 12' },
+                ],
+            },
+        ],
+    },
+    {
+        key: 'sub2',
+        label: 'Navigation Two',
+        icon: <AppstoreOutlined />,
+        children: [
+            { key: '9', label: 'Option 9' },
+            { key: '10', label: 'Option 10' },
+            {
+                key: 'sub3',
+                label: 'Submenu',
+                children: [
+                    { key: '11', label: 'Option 11' },
+                    { key: '12', label: 'Option 12' },
+                ],
+            },
+        ],
+    },
+    {
+        key: 'sub2',
+        label: 'Navigation Two',
+        icon: <AppstoreOutlined />,
+        children: [
+            { key: '9', label: 'Option 9' },
+            { key: '10', label: 'Option 10' },
+            {
+                key: 'sub3',
+                label: 'Submenu',
+                children: [
+                    { key: '11', label: 'Option 11' },
+                    { key: '12', label: 'Option 12' },
+                ],
+            },
+        ],
+    },
+    {
+        key: 'sub2',
+        label: 'Navigation Two',
+        icon: <AppstoreOutlined />,
+        children: [
+            { key: '9', label: 'Option 9' },
+            { key: '10', label: 'Option 10' },
+            {
+                key: 'sub3',
+                label: 'Submenu',
+                children: [
+                    { key: '11', label: 'Option 11' },
+                    { key: '12', label: 'Option 12' },
+                ],
+            },
+        ],
+    },
+    {
+        key: 'sub2',
+        label: 'Navigation Two',
+        icon: <AppstoreOutlined />,
+        children: [
+            { key: '9', label: 'Option 9' },
+            { key: '10', label: 'Option 10' },
+            {
+                key: 'sub3',
+                label: 'Submenu',
+                children: [
+                    { key: '11', label: 'Option 11' },
+                    { key: '12', label: 'Option 12' },
+                ],
+            },
+        ],
+    }
 ];
 
 interface IProps {
