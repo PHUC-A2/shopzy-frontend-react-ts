954e42fd2dfd371b384ea37c5f7cf8f9d3d0d946
PHUC-A2 <phucban297@gmail.com>
2025-08-19 10:26:40 +0700

    #3(Test) Save user login info

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import App from './AppLayout.tsx'
 import './AppLayout.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
-import store from './redux/store.ts';
-
+import { store, persistor } from './redux/store.ts';
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
     </Provider>
 )
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,18 @@
 import { createStore } from "redux"
 import rootReducer from "./reducer/rootReducer";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
-const store = createStore(rootReducer,
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+const store = createStore(persistedReducer,
     (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
     (window as any).__REDUX_DEVTOOLS_EXTENSION__());
-export default store;
+let persistor = persistStore(store)
+
+export { store, persistor }
