eb0192264b11581507bef5c4e9e7b773df132519
PHUC-A2 <phucban297@gmail.com>
2025-09-18 11:25:08 +0700

    #2 update home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,12 @@
-import Sticker01 from '../assets/sticker-01.mp4';
-// import Sticker02 from '../assets/sticker-02.mp4';
-
 const HomePage = () => {
     return (
         <div className='home-container'>
-            <div className="home-video">
-                <video className='home-video-sticker-01' muted autoPlay loop>
-                    <source src={Sticker01} type="video/mp4" />
-                </video>
+            <div className="home-main">
+                <div><h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos quidem minima, magnam cumque cupiditate voluptatum tenetur ad, consequatur explicabo distinctio pariatur illum veritatis molestias harum nesciunt laudantium quo! Velit, optio.</h3></div>
+                <div><h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos quidem minima, magnam cumque cupiditate voluptatum tenetur ad, consequatur explicabo distinctio pariatur illum veritatis molestias harum nesciunt laudantium quo! Velit, optio.</h3></div>
+                <div><h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos quidem minima, magnam cumque cupiditate voluptatum tenetur ad, consequatur explicabo distinctio pariatur illum veritatis molestias harum nesciunt laudantium quo! Velit, optio.</h3></div>
+                <div><h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos quidem minima, magnam cumque cupiditate voluptatum tenetur ad, consequatur explicabo distinctio pariatur illum veritatis molestias harum nesciunt laudantium quo! Velit, optio.</h3></div>
+                <div><h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos quidem minima, magnam cumque cupiditate voluptatum tenetur ad, consequatur explicabo distinctio pariatur illum veritatis molestias harum nesciunt laudantium quo! Velit, optio.</h3></div>
             </div>
             <div className="home-content">
                 <div><h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos quidem minima, magnam cumque cupiditate voluptatum tenetur ad, consequatur explicabo distinctio pariatur illum veritatis molestias harum nesciunt laudantium quo! Velit, optio.</h3></div>
